4e3a5efde3cc1afb7093360cf12ce0b3b68006a6
Faizan Riaz <153673230+Faizanriaz44553@users.noreply.github.com>
2025-08-02 15:13:35 +0000

    project intialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,34 @@
-
-import Navbar from './components/Navbar/Navbar';
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./config/firebase.config";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../.config/firebase.config.js";
 import { useState } from "react";
-import {useEffect} from "react"
-import { Routes, Route } from "react-router-dom";
-import { RoutesPath } from "./routes/RoutesPath";
-import "./App.css"
 
-function App() {
-//  const navigate = useNavigate()
-  //const [user, setUser] = useState(null);
-  //const userAuth = ()=>{
-  
-  //try {
-     //onAuthStateChanged(auth, (user) => {
-  //if (user) {
-   // const uid = user.uid;
-//setUser(user,uid)
-
-  //} else {
-    //setUser(null)
-  //}})
-   //} catch (error) {
-     //console.log(error)
-   //}
-  //}
-
-  //useEffect(()=>{
-    //userAuth()
- // }, navigate())
-
+const App = ()=>{
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true)
+  const navigate = useNavigate()
+  const PrivateRoutes = ()=>{
+    try{
+      onAuthStateChanged(auth, (user)=>{
+        if(user){
+          setUser(user)
+        }
+        else{
+          navigate("/login")
+        }
+      })
+    } catch(error){
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    PrivateRoutes()
+  },navigate())
   return (
-    <div className='app'> 
-      <h1 className='heading'>hello world</h1>
+    <div>
+      {user ? <h1>Hello world</h1> : navigate("/login")}
     </div>
-  );
+  )
 }
-
 export default App;
