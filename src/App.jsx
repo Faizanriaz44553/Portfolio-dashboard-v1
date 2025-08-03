2f9fc9bd11d474ddeeab868bbc9f8b68e98ca012
Faizan Riaz <153673230+Faizanriaz44553@users.noreply.github.com>
2025-08-03 21:58:52 +0000

    project initialization

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,46 @@
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../.config/firebase.config.js";
-import { useState } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Footer from "./components/Footer/Footer.jsx";
+import RoutesPath from "./routes/index.jsx";
+
 
 const App = ()=>{
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true)
   const navigate = useNavigate()
-  const PrivateRoutes = ()=>{
-    try{
-      onAuthStateChanged(auth, (user)=>{
-        if(user){
-          setUser(user)
-        }
-        else{
-          navigate("/login")
-        }
-      })
-    } catch(error){
-      console.log(error)
-    }
-  }
-  useEffect(()=>{
-    PrivateRoutes()
-  },navigate())
+
+       useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setUser(user)
+            } else {
+                navigate('/');
+            }
+        });
+
+        return () => unsubscribe();
+    }, [navigate]);
+    const hideNavbarFooter = location.pathname === "/auth/login" || location.pathname === "/auth/signup";
   return (
     <div>
-      {user ? <h1>Hello world</h1> : navigate("/login")}
+          <div>
+      {!hideNavbarFooter ? <Navbar /> : null} 
+      <div style={{width: "100%" , height: "55px" }}>
+
+      </div>
+
+      <Routes>
+        {RoutesPath.map(({ path, element }, index) => (
+           <Route key={index} path={path} element={element} />
+        ))}
+      </Routes>
+
+      {!hideNavbarFooter ? <Footer /> :null}
+    </div>
     </div>
   )
 }
